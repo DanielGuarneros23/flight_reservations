1d6e5c1169a6140917351db454e0899ca33b6894
hcdante <hectorhugoandradelinares@gmail.com>
2023-08-02 09:44:06 -0600

    Se añade Footer.

--- src/js/main.tsx
+++ src/js/main.tsx
@@ -8,3 +8,60 @@ console.log(a);
 console.log('hello world!!!!!!');
 
 export {}
+
+import axios, { AxiosRequestConfig } from 'axios';
+
+interface Flight {
+  arrivalAirport: {
+    city: string;
+    code: string;
+  };
+  flight_code: string;
+  // Add more properties here if needed
+}
+
+interface ApiResponse {
+  results: Flight[];
+  // Add more properties here if needed
+}
+
+const options: AxiosRequestConfig = {
+  method: 'GET',
+  url: 'https://flight-fare-search.p.rapidapi.com/v2/flights/',
+  params: {
+    from: 'MEX',
+    to: 'ACA',
+    date: '2023-08-12',
+    adult: '1',
+    type: 'economy',
+    currency: 'MXN',
+  },
+  headers: {
+    'X-RapidAPI-Key': '30fb224be1msh4aafd35621b82aap141208jsn051d5840698f',
+    'X-RapidAPI-Host': 'flight-fare-search.p.rapidapi.com',
+  },
+};
+
+function getFlights() {
+  axios
+    .request<ApiResponse>(options)
+    .then((response) => {
+      const data = response.data.results;
+      //console.log(response.data);
+      data.map((element) => {
+        const lugar = element.arrivalAirport.city;
+        const code = element.arrivalAirport.code;
+        const codef = element.flight_code;
+
+        // console.log(lugar);
+        // console.log(code);
+        console.log(codef);
+      });
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+getFlights();
